06ab09eed2214c3f5e8de3fd9f508b4591803acb
Matheus de Fraga Barbosa <matheusdfragabarbosa@gmail.com>
2023-11-25 19:24:46 -0300

    feat: Implementação de CRUD User-Service

--- user-service/src/main/java/com/github/userservice/models/UserModel.java
+++ user-service/src/main/java/com/github/userservice/models/UserModel.java
@@ -1,2 +1,27 @@
-package com.github.userservice.models;public class UserModel {
+package com.github.userservice.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nome;
+
+    private int idade;
+
 }
+
+
