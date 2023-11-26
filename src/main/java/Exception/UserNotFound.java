9ed806f10b5b7f8d2afa6d413a04ece504b42a66
anitaeverywhere <julia.anitasampaio@gmail.com>
2023-11-26 01:09:56 -0300

    feat: implement exception class

---
+++ src/main/java/Exception/UserNotFound.java
@@ -0,0 +1,18 @@
+package Exception;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFound extends RuntimeException {
+    public UserNotFound(String s) {
+    }
+
+    public class UserNotFoundException extends RuntimeException {
+
+        public UserNotFoundException(String message) {
+            super(message);
+        }
+    }
+}
+
+
