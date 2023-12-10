d7e7d8057f1144b1e60ebe7e6e33bd31250d2321
eduardozamit <zamit.eduardo@gmail.com>
2023-12-10 17:09:29 -0300

    feat: criação de tratamentos de exceções + adição de logs de sucesso e logs de erro na user-service.

--- user-service/src/main/java/com/github/userservice/models/UserModel.java
+++ user-service/src/main/java/com/github/userservice/models/UserModel.java
@@ -1,6 +1,9 @@
 package com.github.userservice.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 
@@ -13,10 +16,13 @@ public class UserModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
+    @NotBlank(message = "O nome não pode estar em branco")
     private String name;
 
+    @NotNull(message = "A idade não pode ser nula")
+    @Positive(message = "A idade deve ser um número positivo")
     private int age;
 }
 
--- user-service/src/main/java/com/github/userservice/service/UserService.java
+++ user-service/src/main/java/com/github/userservice/service/UserService.java
@@ -2,39 +2,90 @@ package com.github.userservice.service;
 
 import com.github.userservice.models.UserModel;
 import com.github.userservice.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+
+
 @Service
 public class UserService {
+
     @Autowired
     UserRepository userRepository;
 
-    public List<UserModel> getAllUser() {
-        return userRepository.findAll();
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    public List<UserModel> getAllUsers() {
+        try {
+            List<UserModel> users = userRepository.findAll();
+            logger.info("Lista de usuários obtida com sucesso. Total de usuários: {}", users.size());
+            return users;
+        } catch (Exception e) {
+            logger.error("Erro ao obter todos os usuários na camada de serviço", e);
+            throw new RuntimeException("Erro ao obter todos os usuários", e);
+        }
     }
 
     public Optional<UserModel> getUserById(Long userId) {
-        return userRepository.findById(userId);
+        try {
+            Optional<UserModel> user = userRepository.findById(userId);
+
+            if (user.isPresent()) {
+                logger.info("Usuário encontrado com sucesso na camada de serviço. ID: {}, Nome: {}, Idade: {}", user.get().getId(), user.get().getName(), user.get().getAge());
+            } else {
+                logger.info("Nenhum usuário encontrado para o ID: {} na camada de serviço", userId);
+            }
+
+            return user;
+        } catch (Exception e) {
+            logger.error("Erro ao obter usuário por ID na camada de serviço", e);
+            throw new RuntimeException("Erro ao obter usuário por ID", e);
+        }
     }
 
     public UserModel createUser(UserModel user) {
-        return userRepository.save(user);
+        try {
+            UserModel createdUser = userRepository.save(user);
+            logger.info("Usuário criado com sucesso na camada de serviço. ID: {}, Nome: {}, Idade: {}", createdUser.getId(), createdUser.getName(), createdUser.getAge());
+            return createdUser;
+        } catch (Exception e) {
+            logger.error("Erro ao criar usuário na camada de serviço", e);
+            throw new RuntimeException("Erro ao criar usuário", e);
+        }
     }
 
     public UserModel updateUser(Long userId, UserModel updatedUser) {
-        return userRepository.findById(userId)
-                .map(user -> {
-                    user.setName(updatedUser.getName());
-                    user.setAge((updatedUser.getAge()));
-                    return userRepository.save(user);
-                })
-                .orElse(null);
+        try {
+            Optional<UserModel> userOptional = userRepository.findById(userId);
+
+            if (userOptional.isPresent()) {
+                UserModel user = userOptional.get();
+                user.setName(updatedUser.getName());
+                user.setAge(updatedUser.getAge());
+                userRepository.save(user);
+                logger.info("Usuário atualizado com sucesso na camada de serviço. ID: {}, Nome: {}, Idade: {}", user.getId(), user.getName(), user.getAge());
+                return user;
+            } else {
+                logger.info("Nenhum usuário encontrado para atualização na camada de serviço. ID: {}", userId);
+                return null;
+            }
+        } catch (Exception e) {
+            logger.error("Erro ao atualizar usuário na camada de serviço", e);
+            throw new RuntimeException("Erro ao atualizar usuário", e);
+        }
     }
 
     public void deleteUser(Long userId) {
-        userRepository.deleteById(userId);
+        try {
+            userRepository.deleteById(userId);
+            logger.info("Usuário excluído com sucesso na camada de serviço. ID: {}", userId);
+        } catch (Exception e) {
+            logger.error("Erro ao excluir usuário na camada de serviço", e);
+            throw new RuntimeException("Erro ao excluir usuário", e);
+        }
     }
 }
