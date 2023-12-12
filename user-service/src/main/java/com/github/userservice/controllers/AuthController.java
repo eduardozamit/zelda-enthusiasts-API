a0eb643cbaa5bfd462c913cb3b28a8e617d58b23
Gabriel(Gideon)Martins <gabrielmartinspastoriza3266@gmail.com>
2023-12-12 07:22:18 -0300

    feat(add):adding AuthenticationController and Authentication Service

---
+++ user-service/src/main/java/com/github/userservice/controllers/AuthController.java
@@ -0,0 +1,49 @@
+package com.github.userservice.controllers;
+
+import com.github.userservice.repository.UserRepository;
+import com.github.userservice.user.AuthenticationDTO;
+import com.github.userservice.user.RegisterDTO;
+import com.github.userservice.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository repository;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Validated AuthenticationDTO data){
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody @Validated RegisterDTO data){
+        if (this.repository.findByLogin(data.login()) != null) return ResponseEntity.badRequest().build();
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        User newUser = new User(data.login(), encryptedPassword, data.role());
+
+        this.repository.save(newUser);
+        return ResponseEntity.ok().build();
+
+
+
+    }
+}
---
+++ user-service/src/main/java/com/github/userservice/service/AuthService.java
@@ -0,0 +1,20 @@
+package com.github.userservice.service;
+
+import com.github.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByLogin(username);
+    }
+}
